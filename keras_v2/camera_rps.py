744a0b02db247e0ece3bdfe533e187a60fd26fc0
brandon-tye <brandotie@gmail.com>
2023-07-11 17:58:10 +0100

    Adds countdown and bug fixes.

--- keras_v2/camera_rps.py
+++ keras_v2/camera_rps.py
@@ -3,10 +3,45 @@ import time
 import cv2
 from keras.models import load_model
 import numpy as np
+print("""
+\n*******************LOADING GAME********************\n
+""")
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 options = ["rock","paper","scissors"]
+count_three = """
+                                3333
+                                   3
+                                3333   
+                                   3
+                                3333
+"""
+count_two = """
+                                222222
+                                2    2
+                                    2
+                                   2
+                                 22  
+                                2222222 
+"""
+
+count_one = """
+                                   1
+                                 1 1
+                                1  1
+                                   1
+                                   1
+                                1111111   
+"""
+
+count_go = """
+                                GGGG    OO   ||
+                                G      O  O  ||
+                                G  GG  O  O  ||
+                                G   G  O  O  
+                                GGGGG   OO   !!
+"""
 
 def get_highest_probability() -> int: 
     ret, frame = cap.read()
@@ -31,12 +66,11 @@ def get_prediction(index) -> str:
     
 
 def get_computer_choice() -> str : 
-    random_index = randint(0,len(options))
+    random_index = randint(0,2)
     computer_choice = options[random_index]
     return computer_choice
 
 def get_user_choice() -> str:
-    print(f"Choose one from the following:{options}")
     user_choice = get_prediction(get_highest_probability())
     return user_choice
 
@@ -63,13 +97,37 @@ def get_winner(computer_choice,user_choice):
     else:
         print(f"Computer picked: {computer_choice}\n Player picked: {user_choice}\n It's a tie!")
         return None
-    
+
+def winner_validater(winner):
+    if winner == None:
+        print("No one won this round.")
+    else:
+        print(f"The {winner} has won this battle. The defeated must now surrender")
+
+def choosing_countdown():
+    time.sleep(4)
+    print("\nShow your choice to the camera at the end of this countdown!")
+    time.sleep(1)
+    print(count_three)
+    time.sleep(1)
+    print(count_two)
+    time.sleep(1)
+    print(count_one)
+    time.sleep(1)
+    print(count_go)
+    time.sleep(1)
+
+
+
 def play():
     computer_choice = get_computer_choice()
+    print(f"\nChoose one from the following:{options}")
+    choosing_countdown()
     user_choice = get_user_choice()
     print(f"Player picked: {user_choice}\nComputer picked: {computer_choice}")
     winner = get_winner(computer_choice, user_choice)
-    print(f"The {winner} has won this battle. The defeated must now surrender")
+    time.sleep(1)
+    winner_validater(winner)
 
 play()
     
