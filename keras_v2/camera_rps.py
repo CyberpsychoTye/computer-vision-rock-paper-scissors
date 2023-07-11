aa7b3d4df82b6697a1dd1a41ab2231aee0120bad
brandon-tye <brandotie@gmail.com>
2023-07-11 20:46:42 +0100

    Adds multiple rounds feature

--- keras_v2/camera_rps.py
+++ keras_v2/camera_rps.py
@@ -7,9 +7,12 @@ print("""
 \n*******************LOADING GAME********************\n
 """)
 model = load_model('keras_model.h5')
-cap = cv2.VideoCapture(0)
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+# cap = cv2.VideoCapture(0)
+# data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 options = ["rock","paper","scissors"]
+players = ["Computer","Humanoid Player"]
+computer_score = 0
+player_score = 0
 count_three = """
                                 3333
                                    3
@@ -44,14 +47,17 @@ count_go = """
 """
 
 def get_highest_probability() -> int: 
+    cap = cv2.VideoCapture(0)
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     ret, frame = cap.read()
     resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
     image_np = np.array(resized_frame)
     normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
     data[0] = normalized_image
-    global prediction
     prediction = model.predict(data)
     cv2.imshow('frame', frame)
+    cap.release()
+    cv2.destroyAllWindows()
     return (np.argmax(prediction[0]))
 
 def get_prediction(index) -> str:
@@ -75,35 +81,41 @@ def get_user_choice() -> str:
     return user_choice
 
 def get_winner(computer_choice,user_choice):
-    who_won = ["Computer","Humanoid Player"]
+    
     if computer_choice == "rock" and user_choice == "scissors":
         print("You lost!")
-        return who_won[0]
+        return players[0]
     elif computer_choice == "rock" and user_choice == "paper":
         print("You won!")
-        return who_won[1]
+        return players[1]
     elif computer_choice == "scissors" and user_choice == "paper":
         print("You lost!")
-        return who_won[0]
+        return players[0]
     elif computer_choice == "scissors" and user_choice == "rock":
         print("You won!")
-        return who_won[1]
+        return players[1]
     elif computer_choice == "paper" and user_choice == "rock":
         print("You lost!")
-        return who_won[0]
+        return players[0]
     elif computer_choice == "paper" and user_choice == "scissors":
         print("You won!")
-        return who_won[1]
+        return players[1]
     else:
-        print(f"Computer picked: {computer_choice}\n Player picked: {user_choice}\n It's a tie!")
+        print("It's a tie!")
         return None
 
 def winner_validater(winner):
     if winner == None:
         print("No one won this round.")
+    elif winner == "Computer":
+        print(f"The {winner} has won this round.")
+        global computer_score
+        computer_score += 1
     else:
-        print(f"The {winner} has won this battle. The defeated must now surrender")
-
+        print(f"The {winner} has won this round.")
+        global player_score
+        player_score += 1
+    
 def choosing_countdown():
     time.sleep(4)
     print("\nShow your choice to the camera at the end of this countdown!")
@@ -117,9 +129,14 @@ def choosing_countdown():
     print(count_go)
     time.sleep(1)
 
+def display_scoreboard():
+    print(f"""\n
+                            Player: {player_score}    Computer: {computer_score}
+""")
 
 
-def play():
+
+def play_one_round():
     computer_choice = get_computer_choice()
     print(f"\nChoose one from the following:{options}")
     choosing_countdown()
@@ -128,6 +145,31 @@ def play():
     winner = get_winner(computer_choice, user_choice)
     time.sleep(1)
     winner_validater(winner)
+    time.sleep(2)
+    display_scoreboard()
+    # time.sleep(1)
+    # print("Next round commencing soon. Prepare yourself.....")
+    time.sleep(3)
+    
 
-play()
+
+def play_game():
+    while True:
+        if computer_score == 3 or player_score == 3:
+            if computer_score == 3:
+                print("Computer is superior.\nThe humanoid player has been defeated.\nThe defeated must surrender :(")
+                break
+            else:
+                print("The computer has been defeated.\nHumanoid players are superior, as it always has been.")
+                break
+        else:
+            play_one_round()
+            
+
+play_game()
+
+
+
+
+
     
